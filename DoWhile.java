7ad75bdd2f1424f18b836711996c5b9f71702325
Jonas arcuri <jonasarcuri2020@gmail.com>
2023-09-09 03:58:46 -0300

    Update DoWhile.java

--- DoWhile.java
+++ DoWhile.java
@@ -18,7 +18,7 @@ public class DoWhile {
 			System.out.print("Digite a temperatura em Celsius: ");
 			double c = sc.nextDouble();
 			double f = 9.0 * c / 5.0 + 32.0;
-			System.out.printf("Equiavelente em Fahrenheit: %.1f%n", f);
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", f);
 			System.out.print("Deseja repetir (s/n)?");
 			resposta = sc.next().charAt(0);
 		} while (resposta != 'n');
