83c85d81bd2a151a134651e19e538a40e2418f93
Jonas arcuri <jonasarcuri2020@gmail.com>
2023-09-09 03:58:15 -0300

    Add files via upload
    -commit initial

---
+++ DoWhile.java
@@ -0,0 +1,29 @@
+package EstruturasRepetitivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		char resposta;
+		
+		
+		do {
+			System.out.print("Digite a temperatura em Celsius: ");
+			double c = sc.nextDouble();
+			double f = 9.0 * c / 5.0 + 32.0;
+			System.out.printf("Equiavelente em Fahrenheit: %.1f%n", f);
+			System.out.print("Deseja repetir (s/n)?");
+			resposta = sc.next().charAt(0);
+		} while (resposta != 'n');
+		
+		System.out.println("Obrigado por utilizar!");
+	}
+
+}
